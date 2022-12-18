aefd527dafee5747d7ee35056610c67acda7baea
HoangLeoO <anhlacuaem912001@gmail.com>
2022-12-18 16:30:51 +0700

    function manager-tickker + buy ticker

---
+++ src/app/Controller/manganeController.js
@@ -0,0 +1,94 @@
+const { muntipleMongooseToObject } = require('../../until/mongoose');
+const { mongooseToObject } = require('../../until/mongoose');
+
+const Ticket = require('../Models/ticket');
+const Accout = require('../Models/accouts');
+const UserBuyTicket = require('../Models/userbuyticket');
+
+class manageController {
+
+  // [GET] /manage/showTicketBuy
+
+  showTicketBuy(req, res, next) {
+    UserBuyTicket.find({ deleted: false })
+      .then((userbuyticket) =>
+        res.render('userBuyTicket', {
+          userbuyticket: muntipleMongooseToObject(userbuyticket),
+        })
+      )
+      .catch(next);
+  }
+
+  //[GET] /manage/showTicket
+
+  showTicket(req, res, next) {
+    Ticket.find({})
+      .then((ticket) =>
+        res.render('manageTicket', { ticket: muntipleMongooseToObject(ticket) })
+      )
+      .catch(next);
+  }
+
+  //[GET] /manage/addPage
+  addPage(req, res, next) {
+    res.render('addTicketPage');
+  }
+
+  //[POST] /manage/addTicket
+
+  addTicket(req, res, next) {
+    req.body.image =
+      'https://cdn.bongdaplus.vn/Assets/Media/2022/02/24/62/V-LEAGUE-1.jpg';
+    const ticket = new Ticket(req.body);
+    ticket
+      .save()
+      .then(() => res.redirect('/manage/ticket'))
+      .catch(next);
+  }
+
+  // [GET] /manage/PageUpdateTicket/:id
+
+  PageUpdateTicket(req, res, next) {
+    Ticket.findById(req.params.id)
+      .then((ticket) =>
+        res.render('PageUpdateTicket', { ticket: mongooseToObject(ticket) })
+      )
+      .catch(next);
+  }
+
+  //[PUT] /manage/update/:id
+
+  update(req, res, next) {
+    Ticket.updateOne({ _id: req.params.id }, req.body)
+      .then(() => res.redirect('/manage/ticket'))
+      .catch(next);
+  }
+
+  //[DELETE] /manage/deleteTicket/:id
+  deleteTicket(req, res, next) {
+    Ticket.deleteOne({ _id: req.params.id })
+      .then(() => res.redirect('back'))
+      .catch(next);
+  }
+
+
+  // [GET] /manage/confirmTicket/:id
+  confirmTicket(req, res, next) {
+    UserBuyTicket.delete({ _id: req.params.id })
+      .then(() => res.redirect('back'))
+      .catch(next);
+  }
+
+  // [GET] /manage/listTicket
+  listTicket(req, res, next) {
+    UserBuyTicket.find({ deleted: true })
+      .then((userbuyticket) =>
+        res.render('listTicketBuy', {
+          userbuyticket: muntipleMongooseToObject(userbuyticket),
+        })
+      )
+      .catch(next);
+  }
+}
+
+module.exports = new manageController();
---
+++ src/routes/manage.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const manageController = require('../app/Controllers/manageController');
+
+
+router.get('/addPage', manageController.addPage);
+router.post('/addTicket', manageController.addTicket);
+router.get('/buyticket', manageController.showTicketBuy);
+router.get('/ticket', manageController.showTicket);
+router.get('/PageUpdateTicket/:id', manageController.PageUpdateTicket);
+router.put('/update/:id', manageController.update);
+router.delete('/deleteTicket/:id', manageController.deleteTicket);
+router.get('/search', manageController.search);
+router.get('/confirmTicket/:id', manageController.confirmTicket);
+router.get('/listTicket', manageController.listTicket);
+
+module.exports = router;
